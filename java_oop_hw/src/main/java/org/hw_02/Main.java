e2d7e03d159a7c3f5573fdb99ce073aeedd84536
Demakova Tatiana <demakova_ts@mail.ru>
2023-01-30 00:25:05 +0700

    update project

--- java_oop_hw/src/main/java/org/hw_02/Main.java
+++ java_oop_hw/src/main/java/org/hw_02/Main.java
@@ -1,10 +1,29 @@
 package org.hw_02;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Main {
-
     public static void main(String[] args) {
-        ImmutableCat cat1 = new ImmutableCat("Муся", "бенгал", "мраморный");
-        ImmutableCat cat2 = cat1.setName("Леон");
+        Map<String, String> map = new HashMap<>();
+        map.put("Хороший ключ", "Переменная");
+
+        // Инициализация нашего "иммутабельного" класса
+        AlmostMutableClass almostMutable = new AlmostMutableClass("Это неизменяемый", map);
+        MutableClass mutableClass = new MutableClass("Это изменяемый класс", map);
+
+        // Мы не можем изменять состояние объекта через добавление элементов в полученную map
+        System.out.println("Результат в неизменяемом классе после того, как мы получим map");
+        almostMutable.getFieldMap().put("Плохой ключ", "Другое значение");
+        almostMutable.getFieldMap().keySet().forEach(e -> System.out.println(e));
+
+        System.out.println("Результат в изменяемом классе после того, как мы получим map");
+        mutableClass.getFieldMap().put("Плохой ключ", "Другое значение");
+        mutableClass.getFieldMap().keySet().forEach(e -> System.out.println(e));
+
+        System.out.println("");
+        System.out.println("Результат после изменения исходного map в неизменяемом классе");
+        map.put("Иной ключ", "Иное значение");
+        almostMutable.getFieldMap().keySet().forEach(e -> System.out.println(e));
+        System.out.println("Результат после изменения исходного map в изменяемом классе");
+        mutableClass.getFieldMap().keySet().forEach(e -> System.out.println(e));
     }
-    
 }
---
+++ java_oop_hw/src/main/java/org/hw_02/MutableClass.java
@@ -0,0 +1,17 @@
+package org.hw_02;
+import java.util.Map;
+
+public final class MutableClass {
+    private String field;
+    private Map<String, String> fieldMap;
+    public MutableClass(String field, Map<String, String> fieldMap) {
+        this.field = field;
+        this.fieldMap = fieldMap;
+    }
+    public String getField() {
+        return field;
+    }
+    public Map<String, String> getFieldMap() {
+        return fieldMap;
+    }
+}
